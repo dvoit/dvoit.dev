d923a4841a58a25a8aa923e0a707fdc3414b5803
dillon.voit <dillon.voit@egov.com>
2019-10-05 18:16:08 -0500

    updating background image and css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,11 +32,11 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    <b>dvoit.dev</b>
+                <div class="title">
+                    <b>dvoit . dev</b>
                 </div>
 
-                <div class="links">
+                <div class="links-laravel">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
@@ -44,9 +44,9 @@
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <br><br>
-                    <a href="https://github.com/dvoit"><i class="fab fa-github-alt fa-3x"></i></a>
-                    <a href="https://www.linkedin.com/in/dillon-voit-a392a8175/"><i class="fab fa-linkedin-in fa-3x"></i></a>
-                    <a href="mailto:dillon.voit@gmail.com"><i class="far fa-envelope fa-3x"></i></a>
+                    <a class="links" style="font-size: 13px" href="https://github.com/dvoit"><i class="fab fa-github-alt fa-3x"></i></a>
+                    <a class="links" style="font-size: 13px" href="https://www.linkedin.com/in/dillon-voit-a392a8175/"><i class="fab fa-linkedin-in fa-3x"></i></a>
+                    <a class="links" style="font-size: 13px" href="mailto:dillon.voit@gmail.com"><i class="far fa-envelope fa-3x"></i></a>
                 </div>
             </div>
         </div>
